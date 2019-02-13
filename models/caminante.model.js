01eeb36fed0f559545c4f4be00421e2ea34df3d4
Luis Manuel Torres Treviño <luistorres.compv79@gmail.com>
2019-02-12 20:42:04 -0600

    create the first model: (caminante)

---
+++ models/caminante.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//TODO: complete the schema
+
+let CaminanteSchema = new Schema({
+    name: {type: String, required: true, max: 100},
+    //team: {}
+    address: {type: String, required: true, max: 200},
+    phone: {type: String, required: true, max: 10},
+    age: {type: Number, required: true},
+
+});
